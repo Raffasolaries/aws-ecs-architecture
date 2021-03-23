eedba28a3c991cae268efff97d0103285d3c57b2
Raffasolaries <raffasolaries@gmail.com>
2021-03-23 15:15:02 +0100

    svago deployment update

--- infra-modules/ecs-environment/ecs-fargate/alb_service.tf
+++ infra-modules/ecs-environment/ecs-fargate/alb_service.tf
@@ -16,14 +16,18 @@ resource "aws_lb_target_group" "alb_tg" {
   #   aws_lb.alb
   # ]
   name        = "${var.name_env}-tg"
-  port        = var.app_port
-  protocol    = "TCP"
+  port        = 80
+  protocol    = "HTTP"
   vpc_id      = var.vpc_id
   target_type = "ip"
+
+  tags = {
+    "Name" = "${var.name_env}-tg"
+  }
 }
 
 # Redirect all traffic from the ALB to the target group
-resource "aws_lb_listener_rule" "host_based_routing" {
+resource "aws_lb_listener_rule" "static" {
  depends_on  = [
    aws_lb_target_group.alb_tg
  ]
